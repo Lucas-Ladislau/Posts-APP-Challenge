7fe34e7e7460c2d7ae67be8b9007b24453c865f5
lucas.aguiar <laanderson183@gmail.com>
2023-09-11 12:51:20 -0400

    API interface endpoints

--- app/src/main/java/com/mentoria/blogapp/network/APIInterface.kt
+++ app/src/main/java/com/mentoria/blogapp/network/APIInterface.kt
@@ -1,11 +1,20 @@
 package com.mentoria.blogapp.network
 
+import com.mentoria.blogapp.model.Comment
 import com.mentoria.blogapp.model.Post
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface APIInterface {
 
-    @GET("/posts")
-    fun getPosts(): Response<List<Post>>
+    @GET("posts")
+    suspend fun getPosts(): Response<List<Post>>
+
+    @GET("posts/{id}")
+    suspend fun getPost(@Path("id") id:String): Response<Post>
+
+    @GET("comments")
+    suspend fun getComments(@Query("postId") postId: String): Response<List<Comment>>
 }
