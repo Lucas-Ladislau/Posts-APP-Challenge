0c97d91546f4820cbe59298c85a43d4d88737e5a
lucas.aguiar <laanderson183@gmail.com>
2023-09-11 12:52:06 -0400

    Using the posts endpoint and binding

--- app/src/main/java/com/mentoria/blogapp/MainActivity.kt
+++ app/src/main/java/com/mentoria/blogapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.mentoria.blogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mentoria.blogapp.databinding.ActivityMainBinding
+import com.mentoria.blogapp.viewModel.PostAdapter
+import com.mentoria.blogapp.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var postViewModel: PostViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        postViewModel = ViewModelProvider(this).get(PostViewModel::class.java)
+        postViewModel.getPosts()
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() {
+        postViewModel.postsLiveData.observe(this){
+            binding.postsRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = PostAdapter(it)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mentoria/blogapp/viewModel/PostViewModel.kt
@@ -0,0 +1,36 @@
+package com.mentoria.blogapp.viewModel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.mentoria.blogapp.model.Comment
+import com.mentoria.blogapp.model.Post
+import com.mentoria.blogapp.network.APIClient
+import com.mentoria.blogapp.network.APIInterface
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class PostViewModel : ViewModel() {
+    private val apiInterface = APIClient.getRetrofitInstance("https://jsonplaceholder.typicode.com/")
+    var endpoint = apiInterface.create(APIInterface::class.java)
+    var postsLiveData = MutableLiveData<List<Post>>()
+    var post = MutableLiveData<Post>()
+    var comments = MutableLiveData<List<Comment>>()
+
+
+     fun getPosts(){
+         GlobalScope.launch(Dispatchers.Main) {
+            val response = endpoint.getPosts()
+            if (response.isSuccessful) {
+                val posts = response.body()
+                postsLiveData.postValue(posts)
+            } else {
+                Log.i("Tesx", "Deu BO")
+            }
+        }
+
+    }
+}
